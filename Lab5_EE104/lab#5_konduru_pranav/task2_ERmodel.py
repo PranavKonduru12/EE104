f4bab9eb24381e4dda85696013ff61ff85974376
Pranav Konduru <pskonduru12+gh@gmail.com>
2022-04-08 13:36:03 -0700

    need to fix A_er and A_icu

--- Lab5_EE104/lab#5_konduru_pranav/task2_ERmodel.py
+++ Lab5_EE104/lab#5_konduru_pranav/task2_ERmodel.py
@@ -3,19 +3,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 
-def tank(h,t):
-   # constants
-   c1 = 0.13
-   c2 = 0.20
-   Ac = 2      # m^2
-   # inflow
-   qin = 0.5   # m^3/hr
+def tank(h, t, er1, er2, er3, er4,):
+   # constants 
+   c1 = 10    # Covid patients entering ICU
+   c2 = 5    # Covid patients leaving ICU
+   # Ac = 2      # m^2 (Equivalent to no. of nurses, beds, doctors)
+   A_er = er1 + er2 + er3      # m^2 (Equivalent to no. of nurses (er1), doctors (er2), beds (er3))
+   A_icu = A_er + er4# For ICU, it will be all of the above with respirators, nurses, and beds
+   # inflow of general patients coming into the ER
+   qin = 20   # m^3/hr
    # outflow
    qout1 = c1 * h[0]**0.5
    qout2 = c2 * h[1]**0.5
    # differential equations
-   dhdt1 = (qin   - qout1) / Ac
-   dhdt2 = (qout1 - qout2) / Ac
+   dhdt1 = (qin   - qout1) / A_er
+   dhdt2 = (qout1 - qout2) / A_icu
    # overflow conditions
    if h[0]>=1 and dhdt1>=0:
        dhdt1 = 0
@@ -27,7 +29,13 @@ def tank(h,t):
 # integrate the equations
 t = np.linspace(0,10) # times to report solution
 h0 = [0,0]            # initial conditions for height
-y = odeint(tank,h0,t) # integrate
+#doc = 10
+#nurse = 5
+#bed = 20
+#vent = 30
+er = (10, 5, 20, 30)
+# tank(h0, doc, nurse, beds, vent, t)
+y = odeint(tank,h0, t, er) # integrate
 
 # plot results
 plt.figure(1)
