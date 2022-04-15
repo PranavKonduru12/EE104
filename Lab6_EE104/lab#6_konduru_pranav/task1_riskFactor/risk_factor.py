1f94bfa625312f4b130b57f41bcdf59e0f56aeb5
Pranav Konduru <pranavkonduru@Pranavs-MacBook-Pro.local>
2022-04-15 02:25:32 -0700

    managed to add new column with the risks

--- Lab6_EE104/lab#6_konduru_pranav/task1_riskFactor/risk_factor.py
+++ Lab6_EE104/lab#6_konduru_pranav/task1_riskFactor/risk_factor.py
@@ -4,7 +4,27 @@ from tkinter import W
 #from openpyxl import *
 import pandas as pd
 
-df = pd.read_csv('/Users/pranavkonduru/Local_GIt_Repos/EE104_Projects/Lab6_EE104/lab#6_konduru_pranav/task1_riskFactor/ratings.csv')
+df = pd.read_csv(
+    '/Users/pranavkonduru/Local_GIt_Repos/EE104_Projects/Lab6_EE104/lab#6_konduru_pranav/task1_riskFactor/ratings.csv')
 
-df["new_column"] = " "
-df.to_csv('/Users/pranavkonduru/Local_GIt_Repos/EE104_Projects/Lab6_EE104/lab#6_konduru_pranav/task1_riskFactor/ratings.csv', index=False)
+#df['Risk'] = ' '         # Add nenw column
+# To remove column
+#df.drop('new_column', inplace=True, axis=1)
+df['Risk'] = ' '         # Add nenw column
+def risk_analysis():
+    for index, row in df.iterrows():
+        #print(float(row['COSTTOINCOME']), float(row['ROE']))
+        if float(df.loc[index, 'COSTTOINCOME']) <= 0.5:
+            df.loc[index, 'Risk'] = 'Low'
+        elif float(df.loc[index, 'COSTTOINCOME']) <= 1:
+            df.loc[index, 'Risk'] = 'Medium'
+        else:
+            df.loc[index, 'Risk'] = 'High'
+        #print((row['Risk']))
+
+def main():
+    risk_analysis()
+    df.to_csv(
+        '/Users/pranavkonduru/Local_GIt_Repos/EE104_Projects/Lab6_EE104/lab#6_konduru_pranav/task1_riskFactor/ratings_updated.csv', index=False)
+
+main()
